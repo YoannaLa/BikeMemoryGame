c0b8ca0d009f4bcc5feb0cced11cc80aef2f4d96
YoannaLa <joannaelazarek@yahoo.co.uk>
2021-09-01 17:03:22 +0000

    how to play modal added and styled

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,22 +71,21 @@ const cardArray=[
     }, 
 ]
 
-
 // Get the modal
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementByClassName("myModal-button");
+var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close-btn")[0];
+var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
+// When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (close), close the modal
+// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
