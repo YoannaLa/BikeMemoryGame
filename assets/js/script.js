1ec6b030ac408a795cd75a6837ab8116fb85e98e
YoannaLa <joannaelazarek@yahoo.co.uk>
2021-09-02 22:17:56 +0000

    adding style to game grit

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,7 +27,7 @@ function flipCard() {
 }
 
 function checkForMatch() {
-    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    let isMatch = firstCard.data.type === secondCard.data.type;
 
     isMatch ? disableCards() : unflipCards();
     
@@ -68,12 +68,12 @@ function resetBoard() {
 var sec = 0;
 function pad ( val ) { return val > 9 ? val : "0" + val; }
   setInterval( function(){
-  document.getElementById("seconds").innerHTML=pad(++sec%60);
-  document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
+  document.getElementsByClassName("seconds").innerHTML=pad(++sec%60);
+  document.getElementByClassName("minutes").innerHTML=pad(parseInt(sec/60,10));
     }, 1000);
 
-
 cards.forEach(card => card.addEventListener('click', flipCard));
+
 
 
 // Get the modal
