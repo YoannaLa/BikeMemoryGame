599258967c29c29abf44390e9419977e126d2482
YoannaLa <joannaelazarek@yahoo.co.uk>
2021-09-02 21:38:37 +0000

    adding face up and face down images and vaules to html and style and scripts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,75 +1,80 @@
 // cards array with bike images
-let card = document.getElementsByClassName("card");
+const cards = document.querySelectorAll('.bikecard');
 
-const cardArray=[
-    { 
-        name:'YellowBike',
-        img: 'images/YellowBike.jpg',
-    },
-    { 
-        name:'YellowBike',
-        img: 'images/YellowBike.jpg',
-    },
-    {
-        name:'RepsolBike',
-        img: 'images/RepsolBike.jpg',
-    }, 
-    {
-        name:'RepsolBike',
-        img: 'images/RepsolBike.jpg',
-    }, 
-    {
-        name:'RedBullBike',
-        img: 'images/RedBullBike.jpg',
-    }, 
-    {
-        name:'RedBullBike',
-        img: 'images/RedBullBike.jpg',
-    }, 
-    {
-        name:'PoliceBike',
-        img: 'images/PoliceBike.jpg',
-    },  
-    {
-        name:'PoliceBike',
-        img: 'images/PoliceBike.jpg',
-    },  
-    {
-        name:'OrangeBike',
-        img: 'images/OrangeBike.jpg',
-    },  
-    {
-        name:'OrangeBike',
-        img: 'images/OrangeBike.jpg',
-    },  
-    {
-        name:'GreyBike',
-        img: 'images/GreyBike.jpg',
-    },  
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
 
-    {
-        name:'GreyBike',
-        img: 'images/GreyBike.jpg',
-    },  
+function flipCard() {
 
-    {
-        name:'BarbieBike',
-        img: 'images/BarbieBike.png',
-    }, 
-    {
-        name:'BarbieBike',
-        img: 'images/BarbieBike.png',
-    }, 
-    {
-        name:'BrownBike',
-        img: 'images/BrownBike.jpg',
-    }, 
+    if(lockBoard) return;
+    if (this === firstCard) return;
+    this.classList.toggle('flip');
+
+    if (!hasFlippedCard) {
+
+//first click
+        hasFlippedCard = true;
+        firstCard = this;
+
+        return;
+    } 
+// second click
+        hasFlippedCard = false;
+        secondCard = this;
+
+        checkForMatch();
+}
+
+function checkForMatch() {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+
+    isMatch ? disableCards() : unflipCards();
     
-    {
-        name:'BrownBike',
-        img: 'images/BrownBike.jpg',
-    }, 
-]
+}
+
+function disableCards() {
+
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+function unflipCards() {
+lockBoard = true;
+
+//not a match
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        
+        resetBoard();
+        }, 1100);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    });
+})();
+
+var sec = 0;
+function pad ( val ) { return val > 9 ? val : "0" + val; }
+  setInterval( function(){
+  document.getElementById("seconds").innerHTML=pad(++sec%60);
+  document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
+    }, 1000);
+
+
+cards.forEach(card => card.addEventListener('click', flipCard));
+
 
 // Get the modal
 var modal = document.getElementById("myModal");
